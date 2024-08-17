e85ec2f4e55a1c0a8587d45a7463f30c0c32d4f2
Shinigami1593 <rasaileeayush17@gmail.com>
2024-08-17 23:27:09 +0545

    first commit

---
+++ lilyClothing/src/main/java/com/example/lilyclothing/LilyClothingApplication.java
@@ -0,0 +1,13 @@
+package com.example.lilyclothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilyClothingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LilyClothingApplication.class, args);
+    }
+
+}
---
+++ lilyClothing/src/test/java/com/example/lilyclothing/LilyClothingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lilyclothing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilyClothingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
